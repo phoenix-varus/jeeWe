2069c9cda22ac71d885d60a04e230cbbf5852cad
varus <yanjunfeng2011@live.cn>
2017-09-24 14:57:41 +0800

    功能：代码调整。

--- src/main/java/org/iskycode/jeewe/adm/dao/RolePermissionDao.java
+++ src/main/java/org/iskycode/jeewe/adm/dao/RolePermissionDao.java
@@ -1,7 +1,6 @@
 package org.iskycode.jeewe.adm.dao;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.iskycode.jeewe.adm.entity.AdmUser;
 import org.iskycode.jeewe.sys.dao.BaseDao;
 
 import java.util.Map;
