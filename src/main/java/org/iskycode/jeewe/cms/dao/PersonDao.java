d2e39119dc9e7309663b6cacca7d4862fede4814
varus <yanjunfeng2011@live.cn>
2017-09-26 20:19:39 +0800

    功能：文章、人物数据操作层。

--- src/main/java/org/iskycode/jeewe/cms/dao/PersonDao.java
+++ src/main/java/org/iskycode/jeewe/cms/dao/PersonDao.java
@@ -3,5 +3,5 @@ package org.iskycode.jeewe.cms.dao;
 import org.iskycode.jeewe.cms.entity.CmsPerson;
 import org.iskycode.jeewe.sys.dao.BaseDao;
 
-public interface Person extends BaseDao<CmsPerson> {
+public interface PersonDao extends BaseDao<CmsPerson> {
 }
