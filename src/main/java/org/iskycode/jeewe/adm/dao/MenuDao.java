c43c76d5e00e4bcc66ccb65e3035c176059d3da4
varus <yanjunfeng2011@live.cn>
2017-10-01 19:07:32 +0800

    功能：menu数据操作层。

---
+++ src/main/java/org/iskycode/jeewe/adm/dao/MenuDao.java
@@ -0,0 +1,7 @@
+package org.iskycode.jeewe.adm.dao;
+
+import org.iskycode.jeewe.adm.entity.AdmMenu;
+import org.iskycode.jeewe.sys.dao.BaseDao;
+
+public interface MenuDao extends BaseDao<AdmMenu> {
+}
