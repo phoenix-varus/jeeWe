5037ab82ee3c96a2a59e94cae737e2f48dc67dac
varus <yanjunfeng2011@live.cn>
2017-10-03 21:09:35 +0800

    调整：代码调整。

--- src/main/java/org/iskycode/jeewe/adm/dao/UserRoleDao.java
+++ src/main/java/org/iskycode/jeewe/adm/dao/UserRoleDao.java
@@ -1,17 +1,21 @@
 package org.iskycode.jeewe.adm.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.iskycode.jeewe.sys.dao.BaseDao;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author phoenix
- *
- *         用户角色关联数据操作层
+ * <p>
+ * 用户角色关联数据操作层
  */
 @SuppressWarnings("rawtypes")
 @Mapper
 public interface UserRoleDao extends BaseDao<Map> {
 
+    @Select("<script></script>")
+    public List<Map> findRolesByUserName(String username);
 }
